177ec3093f61d75550c2009615ce9fa8ef6a689b
rasiksilwal013 <rasiksilwal@gmail.com>
2023-12-09 16:46:05 -0500

    move class with method to print content of the move ad metod to prin the contet of the move is created

--- Move.java
+++ Move.java
@@ -1,9 +1,99 @@
 class Move {
-    /* *************************************** */
-    // write your code here
+    private final List<Box> boxes;
 
-    /* *************************************** */
+    public Move(int initialCapacity) {
+        boxes = new ArrayList<>(initialCapacity);
+    }
 
+    // method to add box to the move
+    public void addBox(Box box) {
+        boxes.add(box);
+    }
+
+    // method to print the contents of the move
+    public void print() {
+        System.out.println("The objects of my move are:");
+        for (Box box : boxes) {
+            box.print();
+        }
+    }
+
+    // method to print the contents of the move
+    public void print() {
+        System.out.println("The objects of my move are:");
+        for (Box box : boxes) {
+            box.print();
+        }
+    }
+
+    // method to find box number where an object with a given name is
+    public int find(String itemName) {
+        for (int i = boxes.size() - 1; i >= 0; i--) {
+            int boxNumber = boxes.get(i).find(itemName);
+            if (boxNumber > 0) {
+                return boxNumber;
+            }
+        }
+        return -1;
+    }
+}
+
+class Box {
+    private final List<Object> items;
+    private final int boxNumber;
+
+    // constructor for creating a box capacity and box number
+    public Box(int capacity, int boxNumber) {
+        items = new ArrayList<>(capacity);
+        this.boxNumber = boxNumber;
+    }
+
+    // adding an item
+    public void addItem(Object item) {
+        items.add(item);
+    }
+
+    // printing the contents of the box
+    public void print() {
+        for (Object item : items) {
+            if (item instanceof SingleObject) {
+                System.out.println(((SingleObject) item).getName());
+            } else if (item instanceof Box) {
+                ((Box) item).print();
+            }
+        }
+    }
+
+    //finding box number with a given name of the item
+    public int find(String itemName) {
+        for (Object item : items) {
+            if (item instanceof SingleObject && ((SingleObject) item).getName().equals(itemName)) {
+                return boxNumber;
+            } else if (item instanceof Box) {
+                int result = ((Box) item).find(itemName);
+                if (result > 0) {
+                    return result;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+class SingleObject {
+    private final String name;
+
+    // constructr creating a single object with a specified name
+    public SingleObject(String name) {
+        this.name = name;
+    }
+
+    // getting the name of single object
+    public String getName() {
+        return name;
+    }
+
+    // Main method to demonstrate the usage of the Move, Box, and SingleObject classes
     public static void main(String[] args) {
         // We create a move that will hold 2 main boxes
         Move move = new Move(2);
@@ -51,6 +141,6 @@ class Move {
         move.print();
 
         // We print the number of the outermost cardboard containing the item "scarf"
-        System.out.println("The sarf is in the cardboard number " + move.find("scarf"));
+        System.out.println("The scarf is in the cardboard number " + move.find("scarf"));
     }
 }
